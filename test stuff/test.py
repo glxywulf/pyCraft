2c61bf1b5015dcbf701e91f88ec17d1c5644e665
Brian Li <brian.li@vhhscougars.org>
2022-09-02 10:53:40 -0700

    stuff

--- test stuff/test.py
+++ test stuff/test.py
@@ -2,25 +2,40 @@ from typing import NamedTuple, List, Any, Tuple, Optional
 from numpy import ndarray
 import numpy as np
 
-class Thing(NamedTuple):
+# class Thing(NamedTuple):
     
-    x: str # format for class instance variables 
-    y: str
+#     x: str # format for class instance variables 
+#     y: str
     
-thing = Thing('he', 4)
+# thing = Thing('he', 4)
 
 # print(type(thing.y))
 
-test = np.array([[1,2,3], [1,2,3]])
+# ? ndarray practice stuff
 
-print(test.shape)
+# test = np.array([[1,2,3], [1,2,3]])
 
-blocks: ndarray
+# print(test.shape)
 
-blocks = np.full((3,4,3), 'air') 
-# (x, y, z) = layers, rows, columns
-# (x, y) = rows, columns
+# blocks: ndarray
 
-print(blocks)
-print(blocks.shape)
-print(blocks.size)
+# blocks = np.full((3,4,3), 'air') 
+# # (x, y, z) = layers, rows, columns
+# # (x, y) = rows, columns
+
+# print(blocks)
+# print(blocks.shape)
+# print(blocks.size)
+
+# ? weird syntax thing
+
+thing = Tuple[int, int, int]
+# thing = (1,'hi',3)
+
+# print(repr(thing[0]))
+# print(f'{thing}')
+
+Color = str
+
+print(Color)
+print(thing)
