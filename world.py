2f297427010f18ffa7a38bff885ed1bb4dd7d837
Brian Li <brian.li@vhhscougars.org>
2022-09-19 10:55:39 -0700

    lots of stuff for instance

--- world.py
+++ world.py
@@ -44,6 +44,28 @@ class Chunk:
                     self.lightlvls[x, y, z] = 0
                     bID = 'grass' if (y == 7) else 'stone'
     
+    # set the visible faces of every single block in every chunk and finalize block face states
+    def lightOpt(self, app):
+        print(f"Lighting and optimizing chunk at {self.position}")
+        
+        for xID in range(0, 16):
+            for yID in range(0, 8):
+                for zID in range(0, 16):
+                    self.updateBuried(app, BlockPosition(xID, yID, zID))
+        
+        self.isFinalized = True
+    
+    # make an interable generator object that contains all of the Blocks(specified
+    # by position) and their instances.
+    def iterateInstances(self):
+        if(self.isFinalized and self.isVisible):
+            for (i, instance) in enumerate(self.instances):
+                if(instance != None):
+                    wx = self.position[0] * 16 + (i // 256)
+                    wy = self.position[1] * 16 + (i // 16) % 16
+                    wz = self.position[2] * 16 + (i % 16)
+                    yield (BlockPosition(wx, wy, wz), instance)
+    
     # convert a block coordinate into a UID that specifically denotes a specific block
     def coordToID(self, position):
         (x, y, z) = position
@@ -83,12 +105,83 @@ class Chunk:
             return
         
         gloPos = self.globalBlockPos(bp)
+        
+        for fID in range(0, 12, 2):
+            adjPos = adjaBlockPos(gloPos, fID)
+            
+            if coordOccupied(app, adjPos):
+                self.instances[id][0].visible
+            else:
+                pass
+    
+    # check if a certain coordinate is occupied by a block
+    def coordOccupied(self, bp):
+        # get the block's coordinate position
+        (x, y, z) = bp
+        
+        # check if it's an 'air' block. if not return True since that spot is occupied
+        return self.blocks[x, y, z] != 'air'
     
     def setBlock(self):
         pass
     
     # TODO work on stuff
+
+# get the chunk a certain block is in
+def getChunk(app, bp):
+    # chunk coords are div 16 of the block coords, make it into chunkPos object
+    (chunkX, chunkY, chunkZ) = bp
     
+    chunkX //= 16
+    chunkY //= 16
+    chunkZ //= 16
+    
+    chunk = app.chunks[ChunkPosition(chunkX, chunkY, chunkZ)]
+    
+    # get the block position with respect to the chunk
+    (x, y, z) = bp
+    x %= 16
+    y %= 16
+    z %= 16
+    
+    # return the chunk and blockPos in the chunk
+    return (chunk, BlockPosition(x, y, z))
+
+# helper to get the variables we need for the function inside the Chunk class
+def coordOccupied(app, bp):
+    # if the blockPos is outside of chunk bounds return False
+    if not coordInBound(app, bp):
+        return False
+    
+    (chunk, innerBP) = getChunk(app, bp)
+    
+    return chunk.coordOccupied(innerBP)
+
+def localChunk(bp):
+    (x, y, z) = bp
+    
+    # chunk coords should be what ever the xyz is int divided by 16
+    chunkX = x // 16
+    chunkY = y // 16
+    chunkZ = z // 16
+    
+    # create the ChunkPosition object
+    cPos = ChunkPosition(chunkX, chunkY, chunkZ)
+    
+    # now we need the block's position with respect to the chunk that it's in
+    x %= 16
+    y %= 16
+    z %= 16
+    
+    newBP = BlockPosition(x, y, z)
+    
+    return (cPos, newBP)
+
+# figure out if the coords of the block are within the chunk
+def coordInBound(app, bp):
+    (chunk, block) = localChunk(bp)
+    
+    return chunk in app.chunks
 
 # returning the position of the block that is next to the original block
 # that is touching the face that was inputted
